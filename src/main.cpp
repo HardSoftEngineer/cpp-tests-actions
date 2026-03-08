6dd4f1f0163569c99ed8cc39f238024a09858c2d
HardSoftEngineer <hardsoftengineerperson@gmail.com>
2026-03-08 17:09:57 +0700

    add print release version

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+#ifndef APP_VERSION
+#define APP_VERSION "dev"
+#endif
+
+#ifndef GIT_HASH
+#define GIT_HASH "unknown"
+#endif
+
+#ifndef BUILD_OS
+#define BUILD_OS "unknown"
+#endif
+
 int main() {
-    std::cout << "Hello CI!" << std::endl;
+    std::cout << "Hello CI!\n";
+    std::cout << "Version: " << APP_VERSION << "\n";
+    std::cout << "Commit: " << GIT_HASH << "\n";
+    std::cout << "Built on: " << BUILD_OS << "\n";
+
     return 0;
 }
